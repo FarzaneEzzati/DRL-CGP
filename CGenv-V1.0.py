adafb384b65c8310da0e4488be7f58a4455a90d5
Farzane <ezzati.f.111@gmail.com>
2024-11-12 11:23:50 -0600

    Environment created named as CGenv-V1.0 with a class named Chavatal

--- CGenv-V1.0.py
+++ CGenv-V1.0.py
@@ -1,89 +1,54 @@
 """
-Classic cart-pole system implemented by Rich Sutton et al.
-Copied from http://incompleteideas.net/sutton/book/code/pole.c
-permalink: https://perma.cc/C9ZM-652R
+Environment for Chavatal Gomory Cut agent.
+Created by: Farzane Ezzati and Reza Mirjalili
 """
-
+import copy
 import math
 import random
 
-class CartPole():
+import numpy as np
+
+
+class Chavatal():
     def __init__(self):
-        self._cart_mass = 0.31  # (kg)
-        self._pole_mass = 0.055  # (kg)
-        self._pole_length = 0.4  # (m)
-
-        self.x_threshold = 1.0
-        self.theta_threshold = 12 * 2 * math.pi / 360
-
-        self._state = []
+        self._capacity = 1000
+        self._constraints = 4
+        self._A = [[13, 13, -5, 19, 0,  22],
+                   [10,	1,	5,	-4,	0,	21],
+                   [-1,	12,	16,	19,	0,	32],
+                   [7,	-2,	11,	5,	0,	20]]  # _A = [A | b]
+        self._x = [0.7143, 1, 1, 0.2481, -1]  # _x = [x | -1 ]
+        self._remaining_capacity = copy.copy(self._constraints)
+        self._actions = np.zeros(self._constraints)
+        self._step = 0  # step is integer and between 0 and self._constraints, excluding the latter
+        self._state = [self._step, self._remaining_capacity]
         self._done = True
 
     def reset(self):
         self._step = 0
-        self._cart_position = math.tanh(random.gauss(0.0, 0.01)) * 4.8  # (m)
-        self._cart_velocity = random.uniform(-0.05, 0.05)  # (m/s)
-        initial_pole_angle=random.uniform(-0.05, 0.05)
-        self._pole_angle =  (initial_pole_angle + math.pi) % (2 * math.pi) - math.pi  # (rad)
-        self._pole_angular_velocity = random.uniform(-0.05, 0.05)  # (rad/s)
-
-        # (CartPole-v0 uses numpy.ndarray for state,
-        #  but here returns Python array.)
-        self._state = [self._cart_position, self._cart_velocity, self._pole_angle, self._pole_angular_velocity]
+        self._remaining_capacity = copy.copy(self._constraints)
+        # here it returns Python array.)
+        self._state = [self._step, self._remaining_capacity]
         self._done = False
         return self._state
 
-    def step(self, action: float):
+    def step(self, action: int):
         """
         Args:
-            action: float value between -1.0 and 1.0
+            action: discrete value between 0 and self._capacity
         """
         if self._done:
             raise Exception("Cannot run step() before reset")
 
+        # update state
+        self._actions[self._step] = action  # save the action taken at node step
+        self._remaining_capacity -= action
+        self._state = [self._step, self._remaining_capacity]
+
+        # determine reward
+        reward = np.inner(np.floor(np.divide(action/self._capacity)*self._A[self._step]), self._x)
         self._step += 1
 
-        # Add a small random noise
-        # (The agent won't succeed by applying zero force each time.)
-        force = 1.0 * (action + random.uniform(-0.02, 0.02))
-
-        total_mass = self._cart_mass + self._pole_mass
-        pole_half_length = self._pole_length / 2
-        pole_mass_length = self._pole_mass * pole_half_length
-
-        cosTheta = math.cos(self._pole_angle)
-        sinTheta = math.sin(self._pole_angle)
-
-        temp = (
-            force + pole_mass_length * self._pole_angular_velocity ** 2 * sinTheta
-        ) / total_mass
-        angularAccel = (9.8 * sinTheta - cosTheta * temp) / (
-            pole_half_length
-            * (4.0 / 3.0 - (self._pole_mass * cosTheta ** 2) / total_mass)
-        )
-        linearAccel = temp - (pole_mass_length * angularAccel * cosTheta) / total_mass
-
-        self._cart_position = self._cart_position + 0.02 * self._cart_velocity
-        self._cart_velocity = self._cart_velocity + 0.02 * linearAccel
-
-        self._pole_angle = (
-            self._pole_angle + 0.02 * self._pole_angular_velocity
-        )
-        self._pole_angle = (self._pole_angle + math.pi) % (2 * math.pi) - math.pi
-
-        self._pole_angular_velocity = (
-            self._pole_angular_velocity + 0.02 * angularAccel
-        )
-
-        # (CartPole-v0 uses numpy.ndarray for state,
-        #  but here returns Python array.)
-        self._state = [self._cart_position, self._cart_velocity, self._pole_angle, self._pole_angular_velocity]
-        term = self._state[0] < -self.x_threshold or \
-            self._state[0] > self.x_threshold or \
-            self._state[2] < -self.theta_threshold or \
-            self._state[2] > self.theta_threshold
-        term = bool(term)
-        trunc = (self._step == 500)
-        trunc = bool(trunc)
-        self._done = bool(term or trunc)
-        return self._state, 1.0, term, trunc, {}
+        # check whether the episode is done
+        self._done = 1 if self._remaining_capacity <= 0 else 0
+        return self._state, reward, {}
